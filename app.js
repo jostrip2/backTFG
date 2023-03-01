a9ff77692fb088ad6036a2e2375ebd6a517d7506
jostrip2 <58341561+jostrip2@users.noreply.github.com>
2023-03-01 20:49:13 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const dotenv = require("dotenv");
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+app.set("json spaces", 2);
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+/*
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+*/
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(app.get("port"), () => {
+  console.log(`Listening: http://localhost:${app.get("port")}`);
+});
+
+module.exports = app;
