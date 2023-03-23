aef51d2f9064157cad8c637f8e411cffc89d590f
jostrip2 <58341561+jostrip2@users.noreply.github.com>
2023-03-23 21:40:38 +0100

    limit getAll fora

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,9 +4,7 @@ const { Usuari } = require("../models");
 const authService = require('../services/authService')
 
 const getAllUsers = (req, res) => {
-  Usuari.findAll({
-    limit: 50
-  })
+  Usuari.findAll()
     .then((usuaris) => {
       if (!usuaris) {
         res.status(404).json({ message: 'Usuaris no trobats' });
@@ -64,30 +62,38 @@ const createNewUser = (req, res) => {
 };
 
 const updateOneUser = (req, res) => {
-  User.update(
-    {
-      username: req.body.username,
-      password: req.body.password,
-      email: req.body.email,
-      numMobil: req.body.numMobil,
-      rol: req.body.rol,
-    },
-    {
-      where: {
-        username: req.body.username,
-      },
+  bcrypt.genSalt(10, (err, salt) => {
+    if (!err) {
+      bcrypt.hash(req.body.password, salt, (err, hash) => {
+        if (!err) {
+          User.update(
+            {
+              username: req.body.username,
+              password: hash,
+              email: req.body.email,
+              numMobil: req.body.numMobil,
+              rol: req.body.rol,
+            },
+            {
+              where: {
+                username: req.body.username,
+              },
+            }
+          )
+            .then((usuari) => {
+              if (!usuari) {
+                res.status(404).json({ message: 'Usuari no trobat' });
+              }
+              else res.status(200).json({ data: usuari });
+            })
+            .catch((err) => {
+              console.log(err);
+              res.status(500).send({ message: err });
+            });
+        }
+      })
     }
-  )
-    .then((usuari) => {
-      if (!usuari) {
-        res.status(404).json({ message: 'Usuari no trobat' });
-      }
-      else res.status(200).json({ data: usuari });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send({ message: err });
-    });
+  })
 };
 
 const deleteOneUser = (req, res) => {
